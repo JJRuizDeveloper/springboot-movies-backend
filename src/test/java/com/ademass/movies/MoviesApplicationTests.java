2c2b22f68106aac9ba976355737a477ee33f1914
JuanjoRuiz <juanjose.ruiz.munoz@gmail.com>
2024-05-24 10:47:11 +0200

    init commit

---
+++ src/test/java/com/ademass/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ademass.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
